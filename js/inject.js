6ca41eba4cd55fa945810aded3eb04cdb6c24f94
stevenliuyi <stevenliuyi@gmail.com>
2017-09-06 20:18:35 -0400

    add Bing Dictionary

--- js/inject.js
+++ js/inject.js
@@ -12,11 +12,11 @@
 
     fetchWikipedia(searchQuery, 'en')
     fetchWikipedia(searchQuery, 'zh')
+    fetchBingDictionary(searchQuery)
+
   })
 
   function showResults(results, fillContent, footnote_text, options) {
-    if (results.length === 0) return
-
     let rhs = document.getElementById("rhs_block")
     let site = document.createElement("div")
     site.className = "g rhs-view"
@@ -38,7 +38,7 @@
   function WikipediaContent(results, content, lang) {
     results.map(result => {
       let item = document.createElement("div")
-      item.className = "mod rhs-item"
+      item.className = "rhs-item"
 
       let title = document.createElement("div")
       title.innerHTML = `<a href="https://${lang}.wikipedia.org/wiki?curid=${result.pageid}">${result.title}</a>`
@@ -50,6 +50,36 @@
       item.appendChild(snippet)
       content.appendChild(item)
     })
+  }
+
+  function BingDictionaryContent(results, content) {
+
+    let item = document.createElement("div")
+    item.className = "rhs-item"
+
+    let word = document.createElement("div")
+    word.innerHTML = `<b>${results.word}</b>`
+    
+    let pronunciation = document.createElement("div")
+    pronunciation.className = "grey-text"
+    pronunciation.innerHTML = (results.pronunciation !== null) ?
+      `US [${results.pronunciation.AmE}] UK[${results.pronunciation.BrE}]` :
+      ""
+
+    item.appendChild(word)
+    item.appendChild(pronunciation)
+
+    let item_defs = document.createElement("div")
+    item_defs.className = "rhs-item reduce-space"
+
+    results.defs.map( result => {
+      let definition = document.createElement("div")
+      definition.innerHTML = `<b>${result.pos}</b> ${result.def}`
+      item_defs.appendChild(definition)
+    })
+
+    content.appendChild(item)
+    content.appendChild(item_defs)
   }
 
   function fetchWikipedia(searchQuery, lang) {
@@ -66,12 +96,43 @@
         return
     }
 
-    fetch(`https://${lang}.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchQuery}&srlimit=3&format=json&origin=*`)
+    let params = {
+      'action': 'query',
+      'list': 'search',
+      'srsearch': searchQuery,
+      'srlimit': 3,
+      'format': 'json',
+      'origin': '*'
+    }
+
+    fetch(`https://${lang}.wikipedia.org/w/api.php?${queryParams(params)}`)
       .then(res => res.json())
-      .then(data => showResults(data.query.search,
+      .then(data => (data.query.search.length !== 0) &&
+              showResults(data.query.search,
                                 WikipediaContent,
                                 `from ${lang_text} Wikipedia`,
                                 lang))
   }
+
+  function fetchBingDictionary(searchQuery) {
+    params = {
+      'Word': searchQuery,
+      'Samples': 'false'
+    }
+    fetch(`https://cors-anywhere.herokuapp.com/https://xtk.azurewebsites.net/BingDictService.aspx?${queryParams(params)}`)
+      .then(res => res.json())
+      .then(data => (data.word !== null) &&
+              showResults(data,
+                          BingDictionaryContent,
+                          "from Bing Dictionary"))
+      .catch(e => console.log(e))
+  }
+
+  function queryParams(params) {
+    return Object.keys(params)
+             .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+             .join('&');
+  }
+
 
 })();
